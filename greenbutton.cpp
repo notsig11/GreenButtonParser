6833fe2bac7086f0f94b43fd73d0a09db9bc096a
Lee Helpingstine <sig11@reprehensible.net>
2021-11-13 12:52:41 -0500

    Print some statistics and some minor cleanup.

--- greenbutton.cpp
+++ greenbutton.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <ctime>
+#include <algorithm>
 #include <numeric>
 #include <execution>
 
@@ -7,11 +8,9 @@
 #include <rapidxml/rapidxml_utils.hpp>
 
 struct MeterReading {
-	MeterReading(time_t p_start, uint32_t p_reading) : start(p_start), reading(p_reading) {}
-	time_t start;	
+	MeterReading(time_t start, uint32_t reading) : start(start), reading(reading) {}
+	time_t start;
 	uint32_t reading; // Wh
-
-	uint32_t operator+(const MeterReading& rhs) const { return reading + rhs.reading; }
 };
 
 std::vector<MeterReading> parseGreenButtonXml(std::string filename) {
@@ -23,49 +22,53 @@ std::vector<MeterReading> parseGreenButtonXml(std::string filename) {
 		rapidxml::xml_document<> meter_reads;
 		meter_reads.parse<0>(infile.data());
 		rapidxml::xml_node<>* root = meter_reads.first_node();
-		rapidxml::xml_node<>* reading = root->last_node("entry");	
+		rapidxml::xml_node<>* reading = root->last_node("entry");
 		rapidxml::xml_node<>* content = reading->first_node("content");
 
-		for (auto intervalBlock = content->first_node("IntervalBlock"); intervalBlock; 
+		for (auto intervalBlock = content->first_node("IntervalBlock"); intervalBlock;
 					intervalBlock = intervalBlock->next_sibling()) {
-			auto interval = intervalBlock->first_node("interval");
-//			std::cout << "Interval Block duration: " << interval->first_node("duration")->value() 
+			// TODO: Figure out what to do with interval level duration/start time.  Does it make sense?
+//			auto interval = intervalBlock->first_node("interval");
+//			std::cout << "Interval Block duration: " << interval->first_node("duration")->value()
 //										<< " Interval start time: " << interval->first_node("start")->value() << "\n";
 
 			for (auto intervalReading = intervalBlock->first_node("IntervalReading");
 						intervalReading; intervalReading = intervalReading->next_sibling()) {
 				auto startTime = intervalReading->first_node("timePeriod")->first_node("start");
 				auto reading = intervalReading->first_node("value");
-				
+
 				readings.emplace_back(std::stol(startTime->value()), std::stoi(reading->value()));
-			}	
+			}
 		}
 	}
-	catch (rapidxml::parse_error e) {
+	catch (rapidxml::parse_error &e) {
 		std::cerr << "Error parsing " << filename << " " << e.what() << "\n";
 		exit(-1);
 	}
-	
+
 	return readings;
 }
 
 int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		std::cerr << "Missing filename argument.\n";
+		exit(-1);
+	}
+
 	std::string filename = argv[1];
 	std::vector<MeterReading> readings = parseGreenButtonXml(filename);
 
-/*
-	for (const auto reading : readings) {
-		std::cout << "time: " << std::asctime(std::localtime(&reading.start)) 
-				<< reading.reading << "Wh\n";
-	}
-	*/
-	float average;
-	for (auto r : readings) { average += r.reading; }
-	average /= readings.size();
+	// For now just print some simple statistics...
+	auto average = std::reduce(std::execution::par, readings.begin(), readings.end(), 0,
+				[](int acc, MeterReading b) { return acc + b.reading; }
+			) / static_cast<double>(readings.size());
 
-	std::cout << "Collected " << readings.size() << " readings " << readings.size() / 24 << " days of data average usage " << average << "Wh\n";
+	auto [min, max] = std::minmax_element(std::execution::par, readings.begin(), readings.end(),
+				[](const MeterReading& a, const MeterReading& b) { return a.reading < b.reading; });
+
+	std::cout << "Collected " << readings.size() << " readings "
+			<< readings.size() / 24 << " days of data average usage "
+			<< average << " minmax(" << min->reading <<", " << max->reading << ")Wh\n";
 
 	return 0;
 }
-
-
